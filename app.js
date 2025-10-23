8f634f8977d4ec32031fa1ba5e188dd86a8b578d
JauniKappauni <social.jauni@gmail.com>
2025-10-23 16:10:22 +0200

    Add initial project setup with EJS views and Express dependencies
    - Updated package.json to include Express and EJS as dependencies.
    - Added start script to run the application.
    - Created initial EJS view files: dashboard.ejs, index.ejs, login.ejs, and register.ejs with basic HTML structure.

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+
+app.listen(port, () => {
+  console.log(`App is running on http://localhost${port}`);
+});

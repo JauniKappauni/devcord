43b9515cc08ae40e33cc083a9db0891514b5b304
JauniKappauni <social.jauni@gmail.com>
2025-10-25 15:49:48 +0200

    fix: console log URL formatting in app.js

--- app.js
+++ app.js
@@ -21,5 +21,5 @@ app.get("/dashboard", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`App is running on http://localhost${port}`);
+  console.log(`App is running on http://localhost:${port}`);
 });
